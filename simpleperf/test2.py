150a086f1ad3cf6be86b010c45d5c3ae271023e9
Miski Yusuf <s364593@oslomet.no>
2023-03-16 21:03:08 +0100

    Client fungerer, kan starte med client sin argparse

--- simpleperf/test2.py
+++ simpleperf/test2.py
@@ -38,7 +38,9 @@ def check_ip(val):
         sys.exit()  
 
 
-def handleClient(connection): #A client handler function, this function get's called once a new client joins, and a thread gets created (see main)
+def handleClient(connection, addr ): #A client handler function, this function get's called once a new client joins, and a thread gets created (see main)
+	print(f"A simpleperf client with {addr[0]}:{addr[1]} is connected with {port}")
+
 	while True:
 		msg = connection.recv(1000).decode()   #Decoding recieved message
 		print ("received  message = ", msg)   #Printing the recieved message on the server side
@@ -48,30 +50,19 @@ def handleClient(connection): #A client handler function, this function get's ca
 			break  #Exiting the loop
 		else:
 			melding_send = "Do you want to quit this session?".encode()
+
 	connection.close() #closing the socket
 
 def server(host, port): #main method
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.bind((host,port))	
 	sock.listen()
-	print ('A SIMPLEPERF SERVER IS LISTENING ON PORT 1025',port) #Printing this message on the server
+	print ('A SIMPLEPERF SERVER IS LISTENING ON PORT',port) #Printing this message on the server
 	while True: #always true, this will always loop, and wait for new cleints to connect
 		connectionSocket, addr = sock.accept() #Accepting a new connection
+		thread= threading.Thread(target=handleClient, args =(connectionSocket,addr))
+		thread.start()
 		
-		
-
-def client(host, port):
-	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	clientSocket.connect((host, port))
-	print("Connected to the server.")
-	while True:
-		msg = input("Enter message (type 'bye' to disconnect): ")
-		clientSocket.send(msg.encode())
-		if msg == DISCONNECT:
-			break
-		data = clientSocket.recv(1000).decode()
-		print("Server response: ", data)
-	clientSocket.close()
 
 #lager server 
 parser.add_argument('-s','--server', action='store_true')
@@ -82,51 +73,45 @@ args = parser.parse_args()
 host = args.bind
 port = args.port
 
-if __name__ =="__main__":
-    if args.server:
-        server(host,port)
-    elif args.client:
-          if check_port(port):
-                client(host,port)
 
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Socket settings. THe first says what kind of adress we are looking for,
-
-
-def sendthread():
+def send_thread():
 	while True: 
 		sentence = input("Enter message (type 'bye' to disconnect): ") #asks for client input
-		clientSocket.send(sentence.encode()) #sending the input to the server
+		sock.send(sentence.encode()) #sending the input to the server
 		if (sentence == DISCONNECT): #if the inpout is exit, the client stops
-			msg = sentence.encod()
-			clientSocket.send(msg)
+			msg = sentence.encode()
+			sock.send(msg)
 			break #stops the loop
 		else:
 			msg = sentence.encode()
-			clientSocket.send(msg)
-def thread2(): #another function/thread to listen for messages
+			sock.send(msg)
+
+def receive_thread(sock): #another function/thread to listen for messages
 	msg =""
+	transfer_complete= True
 	while True: 
-		received_line = clientSocket.recv(1024).decode() #When a message is recieved from the client, it is decoded. The message has 1024 bytes.
+		received_line = sock.recv(1000).decode() #When a message is recieved from the client, it is decoded. The message has 1024 bytes.
 		print ('\nFrom Server:', received_line) #prints the menssage recieved from the server
 		if (received_line == DISCONNECT): #if the message is "bye", the thread stops. This is done to prevent infinate looping
-			break #break as before
+			break
 		else:
 			print(msg)
-	clientSocket.close()
+			pass
+	sock.close()
 
-t1 = threading.Thread(target=thread1) #making a thread
 
-host = socket.gethostbyname(socket.gethostname()) #getting the ip name of the server using this function. That makes this code usable for not only one machine
+if args.server:
+    server(host,port)
+elif args.client:        
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serverPort = 12000 #defining the server port (which is the same as in the server part of the code)
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Socket settings. THe first says what kind of adress we are looking for,
-#and the second is the standard option for what we will use the socket for.
-try:
-	clientSocket.connect((host,serverPort)) #trying to connect
-except:
-	print("ConnectionError") #if we can't conenct, an error message is created
-	sys.exit() #and afterwards we exit the system
-
-t1.start() #starting thread 1
-thread2() #and thread 2 (the ) is already running, so we just use a normal function.
-
+    try:
+        sock.connect((host, port))
+        print("A simpleperf client with IP {host}: {port}> is connected with <server IP:port>")
+        t1 = threading.Thread(target=send_thread)
+        t2 = threading.Thread(target=receive_thread, args=(sock,))
+        t1.start()
+        t2.start()
+    except:
+	    print("Feil port nummer endre")
+	
