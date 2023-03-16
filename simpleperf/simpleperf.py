5e3fc3e1e257ebf3f8c911b1dd88b40aea2a492b
Miski Yusuf <s364593@oslomet.no>
2023-03-16 12:21:58 +0100

    IP check fungere

--- simpleperf/simpleperf.py
+++ simpleperf/simpleperf.py
@@ -11,8 +11,14 @@ import sys
 import argparse
 import socket
 import time
+import re
+DISCONNECT = "bye"
+
 """SEERVER MODE"""
 
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 """Her lager jeg en funksjon som sjekker om porten er valid"""
@@ -26,15 +32,16 @@ def check_port(val):
     except ValueError:
         raise argparse.ArgumentTypeError("Expected an integer but you entred a string")
     
-
-
-
-
+def check_ip(val):
+    ip = re.match("[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}",val)
+    if(ip):
+        return True
+    else:
+        print("this is not valid ip-adress")
+        sys.exit()    
 
 def server(host,port):
     #lager en TCP/IP socket og velger type adresse og strem 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
     sock.bind((host,port))
     sock.listen()
     print(f"Hører på on {host}:{port}")
@@ -42,67 +49,62 @@ def server(host,port):
     while True:
         connection, addr =sock.accept()
         print(f"Hører på on {addr}")
-        totalBytes = 0
-        startTid = time.time()
-
         while True:
             data = connection.recv(1000)
             if not data:
                 break
-            totalBytes += len(data)
-        slutt_tid = time.time()
-        bruktTid = slutt_tid - startTid
-        bandwidth = totalBytes / bruktTid / 1000000  # Calculate bandwidth in MB/s
-        print(f"Received {totalBytes} bytes in {bruktTid:.2f} seconds, bandwidth: {bandwidth:.2f} MB/s")
-       
+            connection.sendall(data)
         connection.close()
 
+parser = argparse.ArgumentParser(description="positional arguments", epilog="End of help")
+
+
+#lager server 
+parser.add_argument('-s','--server', action='store_true')
+parser.add_argument('-p','--port', type=check_port)
+parser.add_argument('-b','--bind', type=check_ip)
+args = parser.parse_args()
 
 
 
 if __name__ =="__main__":
-    
-    parser = argparse.ArgumentParser(description="positional arguments", epilog="End of help")
-
-
-
-#lager server 
-    parser.add_argument('-s','--server', action='store_true')
-
-#Port
-    parser.add_argument('-p','--port', type=check_port)
-
-#IP adresse
-
-    parser.add_argument('-b','--bind', type=str, default='heo')
-
-#Lager format
-    parser.add_argument('-f','--format',type=str, default='MB')
-
-
-#parser args
-    args = parser.parse_args()
-
-    
     if args.server:
         print("A simpleperf server is listening on port",args.port)
         host = args.bind
         port = args.port
         server(host,port)
+    elif(args.client):
+        sock.connect(args.bind,args.port)
+
 
 "client"
+"""
+def thread1(sock):  ##hører på 
+    while True:
+        message = sock.recv(1024).decode 
+        print('\nFrom sever:',message)
+        if (message == "bye"):
+            break
 
-def send_melding(host, port, tid_valgt ):
+def threda2():
+    while True:
+        melding = input("input")
+        if melding == DISCONNECT:
+            melding_ut= melding.encode()
+            sock.send(melding_ut)
+            break
+        else:
+            melding_ut = melding.encode()
+            sock.send(melding_ut)
+
+x = '0'*1000  #hvordan man lager 1000 bytes
+print(x)
+"""
+
+def handle_client(host, port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host,port))
 
-    #send data
-    data =  b'\x00' * 1000
-    start_tid = time.time()
-    bytes_sendt = 0
-    while time.time() - start_tid < tid_valgt:
-        client_socket.send(data)
-        bytes_sendt += len(data)
     
     #Send hade melding og vent 
     client_socket.send(b'finish')
@@ -110,21 +112,10 @@ def send_melding(host, port, tid_valgt ):
     if melding_tilbake!= b'ack':
         print('Erro: du har ikke fått tilbake melding fra server ')
     client_socket.close()
-    tid = time.time() - start_tid
-    bandwidth = bytes_sendt / (tid * 1000000)  # In MB/s
-    print(f'Bandwidth: {bandwidth:.2f} MB/s')
-
-parser = argparse.ArgumentParser(description='Simpleperf client')
-parser.add_argument('-c','--client', action='store_true')
-parser.add_argument('-I','--server_ip',  help='IP address of server')
-parser.add_argument('-p','--server_port', type=int, help='Port number of server')
-parser.add_argument('-t','--time_duration', type=int, help='Duration of data generation in seconds')
-args = parser.parse_args()
+ 
 
 
-if args.client:
-        print("A simpleperf client connection to server {args.server_ip}, port {args.server_port}")
-        send_melding(args.server_ip, args.server_port, args.time_duration)
+
 
 
 
